50de8ee153eb9d950fc7a3d116e6f88bd440e474
YiChao Ding <jeffding890430@163.com>
2024-09-30 12:51:03 +0800

    Update showtime.py

--- codetest/showtime.py
+++ codetest/showtime.py
@@ -3,7 +3,7 @@ import pytz
 #设置时区为北京时区
 beijing_tz= pytz.timezone('Asia/shanghai')
 #获取当前时间，并转为北京时间
-current beijing time= datetime.now(beijing tz)
+current_beijing_time= datetime.now(beijing_tz)
 #格式化时间输出
-formatted time = current beijing time.strftime('%-%m-%d %H:%M:%S')
-print("当前北京时间:"，formatted time)
+formatted_time = current_beijing_time.strftime('%y-%m-%d_%H:%M:%S')
+print("当前北京时间:", formatted_time)
